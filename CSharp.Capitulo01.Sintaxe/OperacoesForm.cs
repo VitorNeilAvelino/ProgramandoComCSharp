2343cc50c19742a32d68b9fc56a14c574b6e2759
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-04-07 19:34:52 -0300

    Add project files.

---
+++ CSharp.Capitulo01.Sintaxe/OperacoesForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp.Capitulo01.Sintaxe
+{
+    public partial class OperacoesForm : Form
+    {
+        public OperacoesForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ CSharp.Capitulo01.Variaveis/OperacoesForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp.Capitulo01.Variaveis
+{
+    public partial class OperacoesForm : Form
+    {
+        public OperacoesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void aritmeticasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var x = 1;
+            var texto = "Texto";
+            var data = new DateTime(2021, 04, 07);
+
+            var y = x + texto;
+            var z = data + texto;
+            //var w = x + data;
+
+            MessageBox.Show(y);
+            MessageBox.Show(z);
+        }
+    }
+}
