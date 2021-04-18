e3aef2d993393ef90c0ea5a3c4132886a682d3bb
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-04-18 19:21:31 -0300

    WPF para Correntista

---
+++ Fintech.Correntista.Wpf.5/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Fintech.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Fintech.Correntista.Wpf._5
+{
+    public partial class MainWindow : Window
+    {
+        public List<Cliente> Clientes { get; set; } = new List<Cliente>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopularControles();
+        }
+
+        private void PopularControles()
+        {
+            sexoComboBox.Items.Add(Sexo.Masculino);
+            sexoComboBox.Items.Add(Sexo.Feminino);
+
+            clienteDataGrid.ItemsSource = Clientes;
+        }
+
+        private void IncluirClienteButtonClick(object sender, RoutedEventArgs e)
+        {
+            var endereco = new Endereco();
+            endereco.Logradouro = logradouroTextBox.Text;
+            endereco.Numero = numeroLogradouroTextBox.Text;
+            endereco.Cidade = cidadeTextBox.Text;
+            endereco.Cep = cepTextBox.Text;            
+
+            Cliente cliente = new();
+            cliente.Cpf = cpfTextBox.Text;
+            cliente.Nome = nomeTextBox.Text;
+            cliente.DataNascimento = DateTime.Parse(dataNascimentoTextBox.Text);
+            cliente.Sexo = (Sexo)sexoComboBox.SelectedItem;
+            cliente.EnderecoResidencial = endereco;
+            
+            Clientes.Add(cliente);            
+
+            MessageBox.Show("Cliente cadastrado com sucesso");
+            LimparControlesCliente();
+            clienteDataGrid.Items.Refresh();
+            pesquisaClienteTabItem.Focus();
+        }
+
+        private void LimparControlesCliente()
+        {
+            cpfTextBox.Clear();
+            nomeTextBox.Text = "";
+            dataNascimentoTextBox.Text = null;
+            sexoComboBox.SelectedIndex = -1;
+            logradouroTextBox.Text = string.Empty;
+            numeroLogradouroTextBox.Clear();
+            cidadeTextBox.Clear();
+            cepTextBox.Clear();
+        }
+    }
+}
--- Fintech.Dominio/Endereco.cs
+++ Fintech.Dominio/Endereco.cs
@@ -3,8 +3,13 @@
     public class Endereco
     {
         public string Logradouro { get; set; }
-        public int Numero { get; set; }
+        public string Numero { get; set; }
         public string Cidade { get; set; }
         public string Cep { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Logradouro}, {Numero} - {Cidade} {Cep}";
+        }
     }
 }
